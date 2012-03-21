b5bb98f0acb2869a2b42c9c72e2c1cf78c7a33f4
Mirko Caserta <mirko.caserta@gmail.com>
2012-03-21 17:57:19 +0100

    kata 5 work very much in progress

--- kata-005/src/main/java/Anagrams.java
+++ kata-005/src/main/java/Anagrams.java
@@ -1,6 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Anagrams {
+
+    private List<String> words = new ArrayList<String>();
+
+    public Anagrams() {
+        InputStreamReader in = new InputStreamReader(ClassLoader.getSystemResourceAsStream("common-passwords.txt"));
+        BufferedReader reader = new BufferedReader(in);
+        String line;
+
+        try {
+            while ( (line = reader.readLine()) != null) {
+                words.add(line);
+            }
+        } catch (IOException e) {
+            throw new AnagramException("error reading file: common-passwords.txt", e);
+        }
+    }
 
     /**
      * You need to read the wordlist common-passwords.txt containing one word per line, find all the
@@ -11,7 +33,27 @@ public class Anagrams {
      */
 
     public Set<String> findAnagrams(String word) {
-        return null; //TODO: implement me
+        Set<String> anagrams = new HashSet<String>();
+        
+        return anagrams;
+    }
+    
+    public Set<String> allPermutations(String word) {
+        Set<String> anagrams = new HashSet<String>();
+
+        String permutation = rotate(word);
+
+        return anagrams;
+    }
+
+    private String rotate(String in) {
+        return in.substring(1) + in.charAt(0);
+    }
+
+    public class AnagramException extends RuntimeException {
+        public AnagramException(String s, Throwable throwable) {
+            super(s, throwable);
+        }
     }
 
 }
