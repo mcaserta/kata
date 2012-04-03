5febfb46fd25e68fa1752b8ba05293ef90a6eb34
Mirko Caserta <mirko.caserta@gmail.com>
2012-04-03 14:37:30 +0200

    Implemented kata-004

--- kata-004/src/main/java/QueryStringComposer.java
+++ kata-004/src/main/java/QueryStringComposer.java
@@ -1,21 +1,41 @@
-
 public class QueryStringComposer {
-    
+
     private StringBuffer myQueryString = new StringBuffer();
 
     /**
      * This method use a fluent interface (see: http://en.wikipedia.org/wiki/Fluent_interface)
      * to accept couples key-value useful for create a queryString.
-     *
+     * <p/>
      * if we call addPair(k1, v1) the result must be "?k1=v1"
      * if we call addPair(k1,v1).addPAir(k2,v2) the result must be "?k1=v1&k2=v2
      * and so on.
-     *
+     * <p/>
      * Remember to handle null and empty value, see test class for more.
      *
      * @return a well formed queryString
      */
     public QueryStringComposer addPair(String key, String value) {
-        return null; // TODO: implement me
+        if (isInputValid(key, value)) {
+            if (myQueryString.length() == 0) {
+                myQueryString.append('?');
+            } else {
+                myQueryString.append('&');
+            }
+            myQueryString.append(key);
+            myQueryString.append('=');
+            myQueryString.append(value);
+        }
+
+        return this;
     }
+
+    private boolean isInputValid(String key, String value) {
+        return key != null && !key.isEmpty() && value != null && !value.isEmpty();
+    }
+
+    @Override
+    public String toString() {
+        return myQueryString.toString();
+    }
+
 }
